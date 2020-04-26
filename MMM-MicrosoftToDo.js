99532514d448afbd9ba8258d20c865b91ee3798b
Thomas Bachmann <info@thobach.de>
2020-04-26 22:44:35 +0200

    added linter and security checks

--- MMM-MicrosoftToDo.js
+++ MMM-MicrosoftToDo.js
@@ -1,118 +1,112 @@
-Module.register("MMM-MicrosoftToDo",{
+/*
+global Module
+*/
+Module.register('MMM-MicrosoftToDo', {
 
-    // Override dom generator.
-    getDom: function() {
+  // Override dom generator.
+  getDom: function () {
+    // checkbox icon is added based on configuration
+    var checkbox = this.config.showCheckbox ? '▢&nbsp; ' : ''
 
-      // checkbox icon is added based on configuration
-      var checkbox = this.config.showCheckbox ? "▢&nbsp; " : "";
+    // styled wrapper of the todo list
+    var listWrapper = document.createElement('ul')
+    listWrapper.style.maxWidth = this.config.maxWidth + 'px'
+    listWrapper.style.paddingLeft = '0'
+    listWrapper.style.marginTop = '0'
+    listWrapper.style.listStyleType = 'none'
+    listWrapper.classList.add('small')
 
-      // styled wrapper of the todo list
-      var listWrapper = document.createElement("ul");
-      listWrapper.style.maxWidth = this.config.maxWidth + 'px';
-      listWrapper.style.paddingLeft = '0';
-      listWrapper.style.marginTop = '0';
-      listWrapper.style.listStyleType = 'none';
-      listWrapper.classList.add("small");
+    var listItemsText = ''
 
-      var listItemsText = "";
-
-      // for each entry add styled list items
-      if (this.list.length != 0) {
-        this.list.forEach(element => listItemsText += "<li style=\"list-style-position:inside; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;\">" + checkbox + element.subject + "</li>");
-      }
+    // for each entry add styled list items
+    if (this.list.length !== 0) {
+      this.list.forEach(element => (listItemsText += '<li style="list-style-position:inside; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">' + checkbox + element.subject + '</li>'))
+    } else {
       // otherwise indicate that there are no list entries
-      else {
-        listItemsText += "<li style=\"list-style-position:inside; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;\">" + this.translate("NO_ENTRIES") + "</li>";
-      }
+      listItemsText += '<li style="list-style-position:inside; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">' + this.translate('NO_ENTRIES') + '</li>'
+    }
 
-      // add list items to wrapper
-      listWrapper.innerHTML = listItemsText;
+    // add list items to wrapper
+    listWrapper.innerHTML = listItemsText
 
-      return listWrapper;
-    },
+    return listWrapper
+  },
 
-    getTranslations: function() {
-      return {
-        en: "translations/en.js",
-        de: "translations/de.js"
-      }
-    },
+  getTranslations: function () {
+    return {
+      en: 'translations/en.js',
+      de: 'translations/de.js'
+    }
+  },
 
-    socketNotificationReceived: function (notification, payload) {
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === ('FETCH_INFO_ERROR_' + this.config.id)) {
+      console.error('An error occurred while retrieving the todo list from Microsoft To Do. Please check the logs.')
+      console.error(payload.error)
+      console.error(payload.errorDescription)
+      this.list = [{ subject: 'Error occurred: ' + payload.error + '. Check logs.' }]
 
-      if (notification === ("FETCH_INFO_ERROR_" + this.config.id)) {
+      this.updateDom()
+    }
 
-        console.error('An error occurred while retrieving the todo list from Microsoft To Do. Please check the logs.');
-        console.error(payload.error);
-        console.error(payload.errorDescription);
-        this.list = [ { "subject": "Error occurred: " + payload.error + ". Check logs."} ];
+    if (notification === ('DATA_FETCHED_' + this.config.id)) {
+      this.list = payload
+      console.log(this.name + ' received list of ' + this.list.length + ' items.')
 
-        this.updateDom();
-
-      }
-
-      if (notification === ("DATA_FETCHED_" + this.config.id)) {
-
-        this.list = payload;
-        console.log(this.name + ' received list of ' + this.list.length + ' items.');
-
-        // check if module should be hidden according to list size and the module's configuration
-        if (this.config.hideIfEmpty) {
-          if(this.list.length > 0) {
-            if(this.hidden){
-              this.show()
-            }
-          } else {
-            if(!this.hidden) {
-              console.log(this.name + ' hiding module according to \'hideIfEmpty\' configuration, since there are no tasks present in the list.');
-              this.hide()
-            }
+      // check if module should be hidden according to list size and the module's configuration
+      if (this.config.hideIfEmpty) {
+        if (this.list.length > 0) {
+          if (this.hidden) {
+            this.show()
+          }
+        } else {
+          if (!this.hidden) {
+            console.log(this.name + ' hiding module according to \'hideIfEmpty\' configuration, since there are no tasks present in the list.')
+            this.hide()
           }
         }
-
-        this.updateDom();
-
-      }
-    },
-
-    start: function() {
-
-      // copy module object to be accessible in callbacks
-      var self = this;
-
-      // start with empty list that shows loading indicator
-      self.list = [ { subject: this.translate("LOADING_ENTRIES") } ];
-
-      // decide if a module should be shown if todo list is empty
-      if(self.config.hideIfEmpty === undefined){
-        self.config.hideIfEmpty = false;
       }
 
-      // decide if a checkbox icon should be shown in front of each todo list item
-      if(self.config.showCheckbox === undefined){
-        self.config.showCheckbox = true;
-      }
+      this.updateDom()
+    }
+  },
 
-      // set default max module width
-      if(self.config.maxWidth === undefined){
-        self.config.maxWidth = '450px';
-      }
+  start: function () {
+    // copy module object to be accessible in callbacks
+    var self = this
 
-      // set default limit for number of tasks to be shown
-      if(self.config.itemLimit === undefined){
-        self.config.itemLimit = '200';
-      }
+    // start with empty list that shows loading indicator
+    self.list = [{ subject: this.translate('LOADING_ENTRIES') }]
 
-      // in case there are multiple instances of this module, ensure the responses from node_helper are mapped to the correct module
-      self.config.id = this.identifier;
+    // decide if a module should be shown if todo list is empty
+    if (self.config.hideIfEmpty === undefined) {
+      self.config.hideIfEmpty = false
+    }
 
-      // update tasks every 60s
-      var refreshFunction = function(){
-        self.sendSocketNotification("FETCH_DATA", self.config);
-      }
-      refreshFunction();
-      setInterval(refreshFunction, 60000);
+    // decide if a checkbox icon should be shown in front of each todo list item
+    if (self.config.showCheckbox === undefined) {
+      self.config.showCheckbox = true
+    }
 
-    },
+    // set default max module width
+    if (self.config.maxWidth === undefined) {
+      self.config.maxWidth = '450px'
+    }
 
-});
+    // set default limit for number of tasks to be shown
+    if (self.config.itemLimit === undefined) {
+      self.config.itemLimit = '200'
+    }
+
+    // in case there are multiple instances of this module, ensure the responses from node_helper are mapped to the correct module
+    self.config.id = this.identifier
+
+    // update tasks every 60s
+    var refreshFunction = function () {
+      self.sendSocketNotification('FETCH_DATA', self.config)
+    }
+    refreshFunction()
+    setInterval(refreshFunction, 60000)
+  }
+
+})
--- node_helper.js
+++ node_helper.js
@@ -5,143 +5,127 @@
   therefore we cannot make AJAX calls from the browser without disabling
   webSecurity in Electron.
 */
-var NodeHelper = require("node_helper");
-const request = require("request");
+var NodeHelper = require('node_helper')
+const request = require('request')
 
 module.exports = NodeHelper.create({
 
-    start: function () {
+  start: function () {
+    console.log(this.name + ' helper started ...')
+  },
 
-        console.log(this.name + " helper started ...");
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === 'FETCH_DATA') {
+      this.fetchData(payload)
+    } else {
+      console.log(this.name + ' - Did not process event: ' + notification)
+    }
+  },
 
+  getTodos: function (config) {
+    // copy context to be available inside callbacks
+    var self = this
+
+    // get access token
+    var tokenUrl = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
+    var refreshToken = config.oauth2RefreshToken
+    var data = {
+      client_id: config.oauth2ClientId,
+      scope: 'offline_access user.read tasks.read',
+      refresh_token: refreshToken,
+      grant_type: 'refresh_token',
+      client_secret: config.oauth2ClientSecret
+    }
+    request.post({
+      url: tokenUrl,
+      form: data
     },
+    function (error, response, body) {
+      if (error) {
+        console.error(self.name + ' - Error while requesting access token:')
+        console.error(error)
+      }
 
+      if (body && JSON.parse(body).error) {
+        console.error(self.name + ' - Error while requesting access token:')
+        console.error(JSON.parse(body))
 
-    socketNotificationReceived: function (notification, payload) {
+        self.sendSocketNotification('FETCH_INFO_ERROR_' + config.id, { error: JSON.parse(body).error, errorDescription: JSON.parse(body).error_description })
 
-        if (notification === "FETCH_DATA") {
+        return
+      }
 
-            this.fetchData(payload);
+      const accessTokenJson = JSON.parse(body)
+      var accessToken = accessTokenJson.access_token
 
-        } else {
+      // get tasks
+      var _getTodos = function () {
+        var orderBy = (config.orderBy === 'subject' ? '&$orderby=subject' : '') + (config.orderBy === 'dueDate' ? '&$orderby=duedatetime/datetime' : '')
+        var listUrl = 'https://graph.microsoft.com/beta/me/outlook/taskFolders/' + config.listId + '/tasks?$select=subject,status&$top=' + config.itemLimit + '&$filter=status%20ne%20%27completed%27' + orderBy
 
-          console.log(this.name + " - Did not process event: " + notification);
+        request.get({
+          url: listUrl,
+          headers: {
+            Authorization: 'Bearer ' + accessToken
+          }
+        }, function (error, response, body) {
+          if (error) {
+            console.error(self.name + ' - Error while requesting access token:')
+            console.error(error)
+          }
 
-        }
+          if (body && JSON.parse(body).error) {
+            console.error(self.name + ' - Error while requesting tasks:')
+            console.error(JSON.parse(body).error)
 
-    },
+            self.sendSocketNotification('FETCH_INFO_ERROR_' + config.id, { error: JSON.parse(body).error.code, errorDescription: JSON.parse(body).error.message })
 
-    getTodos: function (config) {
+            return
+          }
 
-        // copy context to be available inside callbacks
-        var self = this;
+          // send tasks to front-end
+          const tasksJson = JSON.parse(body)
+          self.sendSocketNotification('DATA_FETCHED_' + config.id, tasksJson.value)
+        })
+      }
 
-        // get access token
-        var tokenUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
-        var refreshToken = config.oauth2RefreshToken;
-        var data = {
-            client_id: config.oauth2ClientId,
-            scope: "offline_access user.read tasks.read",
-            refresh_token: refreshToken,
-            grant_type: "refresh_token",
-            client_secret: config.oauth2ClientSecret
-        }
-        request.post({
-                url: tokenUrl,
-                form: data
-            },
-            function (error, response, body) {
+      // if list ID was provided, retrieve its tasks
+      if (config.listId !== undefined && config.listId !== '') {
+        _getTodos()
+      } else {
+        // otherwise identify the list ID of the default task list first
+        var taksFoldersUrl = 'https://graph.microsoft.com/beta/me/outlook/taskFolders/?$top=200'
 
-                if (body && JSON.parse(body).error) {
+        request.get({
+          url: taksFoldersUrl,
+          headers: {
+            Authorization: 'Bearer ' + accessToken
+          }
+        }, function (error, response, body) {
+          if (error) {
+            console.log(self.name + ' - Error while requesting task folders:')
+            console.log(error)
 
-                    console.error(self.name + " - Error while requesting access token:");
-                    console.error(JSON.parse(body));
+            self.sendSocketNotification('FETCH_INFO_ERROR', error)
 
-                    self.sendSocketNotification("FETCH_INFO_ERROR_" + config.id, { "error": JSON.parse(body).error, "errorDescription": JSON.parse(body).error_description } );
+            return
+          }
 
-                    return;
-                }
+          // parse response from Microsoft
+          var list = JSON.parse(body)
 
-                const accessTokenJson = JSON.parse(body);
-                var accessToken = accessTokenJson.access_token;
+          // set listID to default task list "Tasks"
+          list.value.forEach(element => element.isDefaultFolder ? (config.listId = element.id) : '')
 
-                // get tasks
-                var _getTodos = function(){
+          // based on new configuration data (listId), get tasks
+          _getTodos()
+        } // function callback for task folders
+        )
+      } // else
+    })
+  },
 
-                  var listUrl = "https://graph.microsoft.com/beta/me/outlook/taskFolders/" + config.listId + "/tasks?$select=subject,status&$top=" + config.itemLimit + "&$filter=status%20ne%20%27completed%27" + (config.orderBy == "subject" ? "&$orderby=subject" : "") + (config.orderBy == "dueDate" ? "&$orderby=duedatetime/datetime" : "")
-
-                  request.get({
-                      url: listUrl,
-                      headers: {
-                          'Authorization': 'Bearer ' + accessToken
-                      }
-                  }, function (error, response, body) {
-
-                      if (body && JSON.parse(body).error) {
-
-                          console.error(self.name + " - Error while requesting tasks:");
-                          console.error(JSON.parse(body).error);
-
-                          self.sendSocketNotification("FETCH_INFO_ERROR_" + config.id, { "error": JSON.parse(body).error.code, "errorDescription": JSON.parse(body).error.message } );
-
-                          return;
-                      }
-
-                      // send tasks to front-end
-                      const tasksJson = JSON.parse(body);
-                      self.sendSocketNotification("DATA_FETCHED_" + config.id, tasksJson.value);
-
-                  });
-                };
-
-                // if list ID was provided, retrieve its tasks
-                if(config.listId !== undefined && config.listId != "") {
-
-                  _getTodos();
-
-                } // if
-                // otherwise identify the list ID of the default task list first
-                else {
-
-                  var taksFoldersUrl = "https://graph.microsoft.com/beta/me/outlook/taskFolders/?$top=200";
-
-                  request.get({
-                      url: taksFoldersUrl,
-                      headers: {
-                          'Authorization': 'Bearer ' + accessToken
-                      }
-                  }, function (error, response, body) {
-
-                      if (error) {
-
-                          console.log(self.name + " - Error while requesting task folders:");
-                          console.log(error);
-
-                          self.sendSocketNotification("FETCH_INFO_ERROR", error);
-
-                          return;
-                      }
-
-                      // parse response from Microsoft
-                      var list = JSON.parse(body);
-
-                      // set listID to default task list "Tasks"
-                      list.value.forEach(element => element.isDefaultFolder ? config.listId = element.id : '' );
-
-                      // based on new configuration data (listId), get tasks
-                      _getTodos();
-
-                  } // function callback for task folders
-                );
-
-              } // else
-
-            });
-    },
-
-    fetchData: function (config) {
-
-        this.getTodos(config);
-
-    },
-});
+  fetchData: function (config) {
+    this.getTodos(config)
+  }
+})
