e63d2bf87de0b846c7ec1158f63f654fc8c066ee
Matt Gerega <gerega@gmail.com>
2021-12-07 19:03:57 -0500

    Added prettier for additional linting

--- MMM-MicrosoftToDo.js
+++ MMM-MicrosoftToDo.js
@@ -1,216 +1,235 @@
 /*
 global Module, Log, moment
 */
-Module.register('MMM-MicrosoftToDo', {
-
+Module.register("MMM-MicrosoftToDo", {
   // Override dom generator.
   getDom: function () {
     // copy module object to be accessible in callbacks
-    var self = this
+    var self = this;
 
     // checkbox icon is added based on configuration
-    var checkbox = this.config.showCheckbox ? '▢ ' : ''
+    var checkbox = this.config.showCheckbox ? "▢ " : "";
 
     // styled wrapper of the todo list
-    var listWrapper = document.createElement('ul')
-    listWrapper.style.maxWidth = this.config.maxWidth + 'px'
-    listWrapper.style.paddingLeft = '0'
-    listWrapper.style.marginTop = '0'
-    listWrapper.style.listStyleType = 'none'
-    listWrapper.classList.add('small')
+    var listWrapper = document.createElement("ul");
+    listWrapper.style.maxWidth = this.config.maxWidth + "px";
+    listWrapper.style.paddingLeft = "0";
+    listWrapper.style.marginTop = "0";
+    listWrapper.style.listStyleType = "none";
+    listWrapper.classList.add("small");
 
     // for each entry add styled list items
     if (this.list.length !== 0) {
       // Define variable itemCounter and set to 0
-      var itemCounter = 0
+      var itemCounter = 0;
       this.list.forEach(function (element) {
         // Get due date array
-        var taskDue = ''
+        var taskDue = "";
         if (self.config.showDueDate === true && element.dueDateTime != null) {
           // timezone is returned as UTC
-          taskDue = Object.values(element.dueDateTime)
+          taskDue = Object.values(element.dueDateTime);
           // converting time zone to browser provided timezone and formatting time according to configuration
-          var taskDueDate = moment.utc(taskDue[0]).tz(Intl.DateTimeFormat().resolvedOptions().timeZone)
+          var taskDueDate = moment
+            .utc(taskDue[0])
+            .tz(Intl.DateTimeFormat().resolvedOptions().timeZone);
           if (self.config.useRelativeDate) {
-            taskDue = taskDueDate.fromNow()
+            taskDue = taskDueDate.fromNow();
           } else {
-            taskDue = taskDueDate.format(self.config.dateFormat)
+            taskDue = taskDueDate.format(self.config.dateFormat);
           }
         }
-        var listItem = document.createElement('li')
-        listItem.style.listStylePosition = 'inside'
-        listItem.style.whiteSpace = 'nowrap'
-        listItem.style.overflow = 'hidden'
-        listItem.style.textOverflow = 'ellipsis'
+        var listItem = document.createElement("li");
+        listItem.style.listStylePosition = "inside";
+        listItem.style.whiteSpace = "nowrap";
+        listItem.style.overflow = "hidden";
+        listItem.style.textOverflow = "ellipsis";
 
         // needed for the fade effect
-        itemCounter += 1
+        itemCounter += 1;
 
         // Create fade effect.
         if (self.config.fade && self.config.fadePoint < 1) {
           if (self.config.fadePoint < 0) {
-            self.config.fadePoint = 0
+            self.config.fadePoint = 0;
           }
-          var startingPoint = self.config.itemLimit * self.config.fadePoint
-          var steps = self.config.itemLimit - startingPoint
+          var startingPoint = self.config.itemLimit * self.config.fadePoint;
+          var steps = self.config.itemLimit - startingPoint;
           if (itemCounter >= startingPoint) {
-            var currentStep = itemCounter - startingPoint
-            listItem.style.opacity = 1 - (1 / steps * currentStep)
+            var currentStep = itemCounter - startingPoint;
+            listItem.style.opacity = 1 - (1 / steps) * currentStep;
           }
         }
 
-        var listItemText = document.createTextNode(checkbox + taskDue + element.title)
-        listItem.appendChild(listItemText)
+        var listItemText = document.createTextNode(
+          checkbox + taskDue + element.title
+        );
+        listItem.appendChild(listItemText);
         // complete task when clicked on it
         if (self.config.completeOnClick) {
           listItem.onclick = function () {
-            self.sendSocketNotification('COMPLETE_TASK', { module: self.data.identifier, listId: element.listId, taskId: element.id, config: self.config })
-          }
+            self.sendSocketNotification("COMPLETE_TASK", {
+              module: self.data.identifier,
+              listId: element.listId,
+              taskId: element.id,
+              config: self.config
+            });
+          };
         }
-        listWrapper.appendChild(listItem)
-      })
+        listWrapper.appendChild(listItem);
+      });
     } else {
       // otherwise indicate that there are no list entries
-      listWrapper.innerHTML += '<li style="list-style-position:inside; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">' + this.translate('NO_ENTRIES') + '</li>'
+      listWrapper.innerHTML +=
+        '<li style="list-style-position:inside; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">' +
+        this.translate("NO_ENTRIES") +
+        "</li>";
     }
-    return listWrapper
+    return listWrapper;
   },
 
   getTranslations: function () {
     return {
-      en: 'translations/en.js',
-      de: 'translations/de.js'
-    }
+      en: "translations/en.js",
+      de: "translations/de.js"
+    };
   },
 
   socketNotificationReceived: function (notification, payload) {
-    if (notification === ('FETCH_INFO_ERROR_' + this.config.id)) {
-      Log.error('An error occurred while retrieving the todo list from Microsoft To Do. Please check the logs.')
-      Log.error(payload.error)
-      Log.error(payload.errorDescription)
-      this.list = [{ subject: 'Error occurred: ' + payload.error + '. Check logs.' }]
+    if (notification === "FETCH_INFO_ERROR_" + this.config.id) {
+      Log.error(
+        "An error occurred while retrieving the todo list from Microsoft To Do. Please check the logs."
+      );
+      Log.error(payload.error);
+      Log.error(payload.errorDescription);
+      this.list = [
+        { subject: "Error occurred: " + payload.error + ". Check logs." }
+      ];
 
-      this.updateDom()
+      this.updateDom();
     }
 
-    if (notification === ('DATA_FETCHED_' + this.config.id)) {
-      this.list = payload
-      Log.info(this.name + ' received list of ' + this.list.length + ' items.')
+    if (notification === "DATA_FETCHED_" + this.config.id) {
+      this.list = payload;
+      Log.info(this.name + " received list of " + this.list.length + " items.");
 
       // check if module should be hidden according to list size and the module's configuration
       if (this.config.hideIfEmpty) {
         if (this.list.length > 0) {
           if (this.hidden) {
-            this.show()
+            this.show();
           }
         } else {
           if (!this.hidden) {
-            Log.info(this.name + ' hiding module according to \'hideIfEmpty\' configuration, since there are no tasks present in the list.')
-            this.hide()
+            Log.info(
+              this.name +
+                " hiding module according to 'hideIfEmpty' configuration, since there are no tasks present in the list."
+            );
+            this.hide();
           }
         }
       }
 
-      this.updateDom()
+      this.updateDom();
     }
 
-    if (notification === ('TASK_COMPLETED_' + this.config.id)) {
-      this.sendSocketNotification('FETCH_DATA', this.config)
+    if (notification === "TASK_COMPLETED_" + this.config.id) {
+      this.sendSocketNotification("FETCH_DATA", this.config);
     }
   },
 
   start: function () {
     // copy module object to be accessible in callbacks
-    var self = this
+    var self = this;
 
     // start with empty list that shows loading indicator
-    self.list = [{ subject: this.translate('LOADING_ENTRIES') }]
-    self.validateConfig()
+    self.list = [{ subject: this.translate("LOADING_ENTRIES") }];
+    self.validateConfig();
 
     // update tasks every based on config refresh
     var refreshFunction = function () {
-      self.sendSocketNotification('FETCH_DATA', self.config)
-    }
-    refreshFunction()
-    setInterval(refreshFunction, self.config.refreshSeconds * 1000)
+      self.sendSocketNotification("FETCH_DATA", self.config);
+    };
+    refreshFunction();
+    setInterval(refreshFunction, self.config.refreshSeconds * 1000);
   },
 
   validateConfig: function () {
-    var self = this
+    var self = this;
 
     // in case there are multiple instances of this module, ensure the responses from node_helper are mapped to the correct module
-    self.config.id = this.identifier
+    self.config.id = this.identifier;
 
     // decide if a module should be shown if todo list is empty
     if (self.config.hideIfEmpty === undefined) {
-      self.config.hideIfEmpty = false
+      self.config.hideIfEmpty = false;
     }
 
     // decide if a checkbox icon should be shown in front of each todo list item
     if (self.config.showCheckbox === undefined) {
-      self.config.showCheckbox = true
+      self.config.showCheckbox = true;
     }
 
     // set default max module width
     if (self.config.maxWidth === undefined) {
-      self.config.maxWidth = '450px'
+      self.config.maxWidth = "450px";
     }
 
     // set default limit for number of tasks to be shown
     if (self.config.itemLimit === undefined) {
-      self.config.itemLimit = '200'
+      self.config.itemLimit = "200";
     }
 
     // set default task completion on click to false
     if (self.config.completeOnClick === undefined) {
-      self.config.completeOnClick = false
+      self.config.completeOnClick = false;
     }
 
     // decide if the task due date should be shown in front of each todo list item, if it exists
     if (self.config.showDueDate === undefined) {
-      self.config.showDueDate = false
+      self.config.showDueDate = false;
     }
 
     // format to display the due date
     if (self.config.dateFormat === undefined) {
-      self.config.dateFormat = 'ddd MMM Do [ - ]'
+      self.config.dateFormat = "ddd MMM Do [ - ]";
     }
 
     // set default refresh interval to 60s
     if (self.config.refreshSeconds === undefined) {
-      self.config.refreshSeconds = 60
+      self.config.refreshSeconds = 60;
     }
 
     // set default useRelativeDate to false
     if (self.config.useRelativeDate === undefined) {
-      self.config.useRelativeDate = false
+      self.config.useRelativeDate = false;
     }
 
     // set default plannedTasks settings
     if (self.config.plannedTasks === undefined) {
       self.config.plannedTasks = {
         enable: false
-      }
+      };
     }
 
     // By default, don't ignore any lists
     if (self.config.plannedTasks.ignoreLists === undefined) {
-      self.config.plannedTasks.ignoreLists = []
+      self.config.plannedTasks.ignoreLists = [];
     }
 
     // by default, only look at tasks 2 weeks out
     if (self.config.plannedTasks.duration === undefined) {
       self.config.plannedTasks.duration = {
         weeks: 2
-      }
+      };
     }
 
     if (self.config.listId !== undefined) {
-      Log.error(`${self.name} - configuration parameter listId is invalid, please use listName instead.`)
-      return false
+      Log.error(
+        `${self.name} - configuration parameter listId is invalid, please use listName instead.`
+      );
+      return false;
     }
 
-    return true
+    return true;
   }
-
-})
+});
--- node_helper.js
+++ node_helper.js
@@ -5,172 +5,201 @@
   therefore we cannot make AJAX calls from the browser without disabling
   webSecurity in Electron.
 */
-var NodeHelper = require('node_helper')
-const fetch = require('node-fetch')
-const Log = require('logger')
-const { add, formatISO, compareAsc, parse } = require('date-fns')
+var NodeHelper = require("node_helper");
+const fetch = require("node-fetch");
+const Log = require("logger");
+const { add, formatISO, compareAsc, parse } = require("date-fns");
 module.exports = NodeHelper.create({
-
   start: function () {
-    Log.info(`${this.name} node_helper started ...`)
+    Log.info(`${this.name} node_helper started ...`);
   },
 
   socketNotificationReceived: function (notification, payload) {
-    if (notification === 'FETCH_DATA') {
-      this.fetchData(payload)
-    } else if (notification === 'COMPLETE_TASK') {
-      this.completeTask(payload.listId, payload.taskId, payload.config)
+    if (notification === "FETCH_DATA") {
+      this.fetchData(payload);
+    } else if (notification === "COMPLETE_TASK") {
+      this.completeTask(payload.listId, payload.taskId, payload.config);
     } else {
-      Log.warn(`${this.name} - did not process event: ${notification}`)
+      Log.warn(`${this.name} - did not process event: ${notification}`);
     }
   },
 
   completeTask: function (listId, taskId, config) {
     // copy context to be available inside callbacks
-    const self = this
+    const self = this;
 
-    var patchUrl = `https://graph.microsoft.com/v1.0/me/lists/${listId}/tasks/${taskId}`
+    var patchUrl = `https://graph.microsoft.com/v1.0/me/lists/${listId}/tasks/${taskId}`;
 
     const updateBody = {
       id: taskId,
-      status: 'completed'
-    }
+      status: "completed"
+    };
 
     fetch(patchUrl, {
-      method: 'PATCH',
+      method: "PATCH",
       body: JSON.stringify(updateBody),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authentication: `Bearer ${self.accessToken}`
       }
-    }).then(self.checkFetchStatus)
+    })
+      .then(self.checkFetchStatus)
       .then((response) => response.json())
       .then(self.checkBodyError)
       .then((responseJson) => {
-        self.sendSocketNotification(`TASK_COMPLETED_${config.id}`, responseJson)
+        self.sendSocketNotification(
+          `TASK_COMPLETED_${config.id}`,
+          responseJson
+        );
       })
-      .error((error) => self.logError('COMPLETE_TASK_ERROR', error))
+      .error((error) => self.logError("COMPLETE_TASK_ERROR", error));
   },
 
   getTodos: function (config) {
     // copy context to be available inside callbacks
-    const self = this
+    const self = this;
 
     // get access token
-    var tokenUrl = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
-    var refreshToken = config.oauth2RefreshToken
-    const form = new URLSearchParams()
-    form.append('client_id', config.oauth2ClientId)
-    form.append('scope', 'offline_access user.read ' + (config.completeOnClick ? 'tasks.readwrite' : 'tasks.read'))
-    form.append('refresh_token', refreshToken)
-    form.append('grant_type', 'refresh_token')
-    form.append('client_secret', config.oauth2ClientSecret)
+    var tokenUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+    var refreshToken = config.oauth2RefreshToken;
+    const form = new URLSearchParams();
+    form.append("client_id", config.oauth2ClientId);
+    form.append(
+      "scope",
+      "offline_access user.read " +
+        (config.completeOnClick ? "tasks.readwrite" : "tasks.read")
+    );
+    form.append("refresh_token", refreshToken);
+    form.append("grant_type", "refresh_token");
+    form.append("client_secret", config.oauth2ClientSecret);
 
     fetch(tokenUrl, {
-      method: 'POST',
+      method: "POST",
       body: form
-    }).then(self.checkFetchStatus)
+    })
+      .then(self.checkFetchStatus)
       .then((response) => response.json())
       .then(self.checkBodyError)
       .then((accessTokenJson) => {
-        var accessToken = accessTokenJson.access_token
-        self.accessToken = accessToken
-        self.fetchList(accessToken, config)
+        var accessToken = accessTokenJson.access_token;
+        self.accessToken = accessToken;
+        self.fetchList(accessToken, config);
       })
       .catch((error) => {
-        self.logError(`FETCH_INFO_ERROR_${config.id}`, error)
-      })
+        self.logError(`FETCH_INFO_ERROR_${config.id}`, error);
+      });
   },
   fetchList: function (accessToken, config) {
-    const self = this
+    const self = this;
 
-    var filterClause = ''
-    const hasListNameInConfig = (config.listName !== undefined && config.listName !== '')
+    var filterClause = "";
+    const hasListNameInConfig =
+      config.listName !== undefined && config.listName !== "";
     // filter by displayName, otherwise, get all the lists
     if (hasListNameInConfig) {
       // Get the list ID based on name
-      filterClause = `displayName eq '${config.listName}'`
+      filterClause = `displayName eq '${config.listName}'`;
     }
 
-    filterClause = encodeURIComponent(filterClause).replaceAll("'", '%27')
+    filterClause = encodeURIComponent(filterClause).replaceAll("'", "%27");
 
-    var filter = ''
-    if (filterClause !== '') {
-      filter = `&$filter=${filterClause}`
+    var filter = "";
+    if (filterClause !== "") {
+      filter = `&$filter=${filterClause}`;
     }
 
     // get ID of task folder
-    var getListUrl = `https://graph.microsoft.com/v1.0/me/todo/lists/?$top=200${filter}`
+    var getListUrl = `https://graph.microsoft.com/v1.0/me/todo/lists/?$top=200${filter}`;
     fetch(getListUrl, {
-      method: 'get',
+      method: "get",
       headers: {
-        Authorization: 'Bearer ' + accessToken
+        Authorization: "Bearer " + accessToken
       }
-    }).then(self.checkFetchStatus)
+    })
+      .then(self.checkFetchStatus)
       .then((response) => response.json())
       .then(self.checkBodyError)
       .then((responseData) => {
-        var listIds = []
+        var listIds = [];
         if (config.plannedTasks.enable) {
           //  Filter out any lists that are in the `ignoreLists` collection
           listIds = responseData.value
-            .filter(list => config.plannedTasks.ignoreLists.findIndex((ignore) => ignore === list.displayName) === -1)
-            .map((list) => list.id)
+            .filter(
+              (list) =>
+                config.plannedTasks.ignoreLists.findIndex(
+                  (ignore) => ignore === list.displayName
+                ) === -1
+            )
+            .map((list) => list.id);
         } else if (responseData.value.length > 0) {
           if (!hasListNameInConfig) {
             // If there is no list name in the config and it's not showPlannedTasks, get the default list
-            const list = responseData.value.find((element) => element.wellknownListName === 'defaultList')
+            const list = responseData.value.find(
+              (element) => element.wellknownListName === "defaultList"
+            );
             if (list) {
-              listIds.push(list.id)
+              listIds.push(list.id);
             }
           } else {
-            listIds.push(responseData.value[0].id)
+            listIds.push(responseData.value[0].id);
           }
         }
 
         if (listIds.length > 0) {
-          self.getTasks(accessToken, config, listIds)
+          self.getTasks(accessToken, config, listIds);
         } else {
-          self.logError(`FETCH_INFO_ERROR_${config.id}`, { error: `"${config.listName}" task folder not found`, errorDescription: `The task folder "${config.listName}" could not be found.` })
+          self.logError(`FETCH_INFO_ERROR_${config.id}`, {
+            error: `"${config.listName}" task folder not found`,
+            errorDescription: `The task folder "${config.listName}" could not be found.`
+          });
         }
       }) // function callback for task folders
-      .catch(error => self.logError(`FETCH_INFO_ERROR_${config.id}`, error))
+      .catch((error) => self.logError(`FETCH_INFO_ERROR_${config.id}`, error));
   },
   fetchData: function (config) {
-    this.getTodos(config)
+    this.getTodos(config);
   },
   getTasks: function (accessToken, config, listIds) {
-    const self = this
-    const formatString = "yyyy-MM-dd'T'HH:mm:ss.SSSS"
-    Log.info(`[MMM-MicrosoftToDo] - Retrieving Tasks for ${listIds.length} list(s)`)
+    const self = this;
+    const formatString = "yyyy-MM-dd'T'HH:mm:ss.SSSS";
+    Log.info(
+      `[MMM-MicrosoftToDo] - Retrieving Tasks for ${listIds.length} list(s)`
+    );
 
     // TODO: Iterate through ALL the lists.  If showplannedtasks, filter out those without
     var promises = listIds.map((listId) => {
-      var orderBy = (config.orderBy === 'subject' ? '&$orderby=title' : '') + (config.orderBy === 'dueDate' ? '&$orderby=duedatetime/datetime' : '')
-      var filterClause = "status ne 'completed'"
+      var orderBy =
+        (config.orderBy === "subject" ? "&$orderby=title" : "") +
+        (config.orderBy === "dueDate" ? "&$orderby=duedatetime/datetime" : "");
+      var filterClause = "status ne 'completed'";
       if (config.plannedTasks.enable) {
-        var pastDate = formatISO(add(Date.now(), config.plannedTasks.duration))
-        filterClause += ` and duedatetime/datetime lt '${pastDate}' and duedatetime/datetime ne null`
+        var pastDate = formatISO(add(Date.now(), config.plannedTasks.duration));
+        filterClause += ` and duedatetime/datetime lt '${pastDate}' and duedatetime/datetime ne null`;
       }
 
-      filterClause = encodeURIComponent(filterClause).replaceAll("'", '%27')
-      var listUrl = `https://graph.microsoft.com/v1.0/me/todo/lists/${listId}/tasks?$top=${config.itemLimit}&$filter=${filterClause}${orderBy}`
-      Log.debug(`[MMM-MicrosoftToDo] - Retrieving Tasks ${listUrl}`)
+      filterClause = encodeURIComponent(filterClause).replaceAll("'", "%27");
+      var listUrl = `https://graph.microsoft.com/v1.0/me/todo/lists/${listId}/tasks?$top=${config.itemLimit}&$filter=${filterClause}${orderBy}`;
+      Log.debug(`[MMM-MicrosoftToDo] - Retrieving Tasks ${listUrl}`);
       return fetch(listUrl, {
-        method: 'get',
+        method: "get",
         headers: {
           Authorization: `Bearer ${accessToken}`
         }
-      }).then(self.checkFetchStatus)
+      })
+        .then(self.checkFetchStatus)
         .then((response) => response.json())
         .then(self.checkBodyError)
         .then((responseData) => {
-          var tasks = []
-          if (responseData.value !== null && responseData.value !== undefined && responseData.value.length > 0) {
+          var tasks = [];
+          if (
+            responseData.value !== null &&
+            responseData.value !== undefined &&
+            responseData.value.length > 0
+          ) {
             tasks = responseData.value.map((element) => {
-              var parsedDate
+              var parsedDate;
               if (element.dueDateTime !== undefined) {
-                parsedDate = parse(element.dueDateTime.dateTime, formatString)
+                parsedDate = parse(element.dueDateTime.dateTime, formatString);
               }
               return {
                 id: element.id,
@@ -178,57 +207,58 @@ module.exports = NodeHelper.create({
                 dueDateTime: element.dueDateTime,
                 listId: config._listId,
                 parsedDate: parsedDate
-              }
-            })
+              };
+            });
           }
-          return tasks
+          return tasks;
         }) // function callback for task folders
-        .catch(self.logError)
-    })
+        .catch(self.logError);
+    });
 
-    Log.debug(`[MMM-MicrosoftToDo] - waiting on ${promises.length} promises`)
-    Promise.all(promises).then((taskArray) => {
-      const returnTasks = []
-      taskArray.forEach(element => {
-        if (element !== null && element !== undefined && element.length > 0) {
-          element.forEach((task) => returnTasks.push(task))
-        }
+    Log.debug(`[MMM-MicrosoftToDo] - waiting on ${promises.length} promises`);
+    Promise.all(promises)
+      .then((taskArray) => {
+        const returnTasks = [];
+        taskArray.forEach((element) => {
+          if (element !== null && element !== undefined && element.length > 0) {
+            element.forEach((task) => returnTasks.push(task));
+          }
+        });
+
+        returnTasks.sort(self.taskSortCompare);
+
+        Log.info(`[MMM-MicrosoftToDo] - returning ${returnTasks.length} tasks`);
+        self.sendSocketNotification(`DATA_FETCHED_${config.id}`, returnTasks);
       })
-
-      returnTasks.sort(self.taskSortCompare)
-
-      Log.info(`[MMM-MicrosoftToDo] - returning ${returnTasks.length} tasks`)
-      self.sendSocketNotification(`DATA_FETCHED_${config.id}`, returnTasks)
-    }).catch((error) => {
-      Log.info(`[MMM-MicrosoftToDo] - error ${error}`)
-    }
-    )
+      .catch((error) => {
+        Log.info(`[MMM-MicrosoftToDo] - error ${error}`);
+      });
   },
   taskSortCompare: function (firstTask, secondTask) {
     if (firstTask.parsedDate === undefined) {
-      return 1
+      return 1;
     }
     if (secondTask.parsedDate === undefined) {
-      return -1
+      return -1;
     }
-    return compareAsc(firstTask.parsedDate, secondTask.parsedDate)
+    return compareAsc(firstTask.parsedDate, secondTask.parsedDate);
   },
 
   checkFetchStatus: function (response) {
     if (response.ok) {
-      return response
+      return response;
     } else {
-      throw Error(response.statusText)
+      throw Error(response.statusText);
     }
   },
   checkBodyError: function (json) {
     if (json && json.error) {
-      throw Error(json.error)
+      throw Error(json.error);
     }
-    return json
+    return json;
   },
   logError: function (notificationName, error) {
-    Log.error(`[MMM-MicrosoftToDo]: ${JSON.stringify(error)}`)
-    self.sendSocketNotification(notificationName, error)
+    Log.error(`[MMM-MicrosoftToDo]: ${JSON.stringify(error)}`);
+    self.sendSocketNotification(notificationName, error);
   }
-})
+});
